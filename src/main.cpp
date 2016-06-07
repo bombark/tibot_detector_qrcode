24069d3303f9bf5e5dbca840583a7d6d92234a23
Felipe Bombardelli <felipebombardelli@gmail.com>
2016-06-07 13:56:36 -0300

    Added zbar and organize algorithm to tibot standart

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,56 @@
- #include <opencv2/highgui/highgui.hpp>  
- #include <opencv2/imgproc/imgproc.hpp>  
- #include <zbar.h>
- #include <iostream>
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>  
+#include <opencv2/imgproc/imgproc.hpp>  
+#include <zbar.h>
+
+
+using namespace std;
+using namespace zbar;
 
 
 
-//g++ main.cpp /usr/local/include/ /usr/local/lib/ -lopencv_highgui.2.4.8 -lopencv_core.2.4.8  
 int main(int argc, char* argv[])  {  
-	VideoCapture cap(0);
-	if (!cap.isOpened()){
-		cout << "Cannot open the video cam" << endl;  
-		return -1;  
-	}
+	ImageScanner scanner;
+	scanner.set_config(ZBAR_NONE, ZBAR_CFG_ENABLE, 1);
 
+	char buf[64];
+	while ( scanf("%s", buf) ){  
+		cv::Mat frame, grey;
 
-	ImageScanner scanner;   
-	scanner.set_config(ZBAR_NONE, ZBAR_CFG_ENABLE, 1);   
-	double dWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH); //get the width of frames of the video  
-	double dHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video  
-	cout << "Frame size : " << dWidth << " x " << dHeight << endl;  
-	namedWindow("MyVideo",CV_WINDOW_AUTOSIZE); //create a window called "MyVideo"  
+		if ( frame.type() == CV_8UC1 ){
+			cv::cvtColor(frame,grey,CV_BGR2GRAY);  
+		} else {
+			grey = frame;
+		}
 
-	while (1){  
-		Mat frame;  
-		bool bSuccess = cap.read(frame); // read a new frame from video  
-		if (!bSuccess){  
-			cout << "Cannot read a frame from video stream" << endl;  
-			break;  
-		}  
-
-		Mat grey;  
-		cvtColor(frame,grey,CV_BGR2GRAY);  
-		int width = frame.cols;
+		int width  = frame.cols;
 		int height = frame.rows;
 
-		uchar *raw = (uchar *)grey.data;
+		uchar *raw = (uchar *) grey.data;
 		Image image(width, height, "Y800", raw, width * height);   
 		int n = scanner.scan(image);
 		for(Image::SymbolIterator symbol = image.symbol_begin(); symbol != image.symbol_end(); ++symbol){
-			vector<Point> vp;   
-			// do something useful with results   
-			cout << "decoded " << symbol->get_type_name() << " symbol \"" << symbol->get_data() << '"' <<" "<< endl;   
+			/*vector<cv::Point> vp;
+			//cout << "decoded " << symbol->get_type_name() << " symbol \"" << symbol->get_data() << '"' <<" "<< endl;   
 			int n = symbol->get_location_size();   
 			for(int i=0;i<n;i++){   
-				vp.push_back(Point(symbol->get_location_x(i),symbol->get_location_y(i)));   
+				vp.push_back(cv::Point(symbol->get_location_x(i),symbol->get_location_y(i)));   
 			}
-			RotatedRect r = minAreaRect(vp);
-			Point2f pts[4];
+			cv::RotatedRect r = minAreaRect(vp);
+			cv::Point2f pts[4];
 			r.points(pts);
 			for(int i=0;i<4;i++){
-				line(frame,pts[i],pts[(i+1)%4],Scalar(255,0,0),3);
-			}
+				line(frame,pts[i],pts[(i+1)%4],cv::Scalar(255,0,0),3);
+			}*/
 		}
-		
-		imshow("MyVideo", frame);
-		if (waitKey(30) == 27){  
+
+		/*cv::imshow("MyVideo", frame);
+		if (cv::waitKey(30) == 27){  
 			cout << "esc key is pressed by user" << endl;
 			break;
-		}
-		
+		}*/
+
 	}
-	return 0;  
+	return 0;
 } 
